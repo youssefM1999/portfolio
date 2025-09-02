61526a5c43002677238c6d0a00521b1f3ccabda7
Youssef Kamel <youssef@Youssefs-MacBook-Air.local>
2025-09-01 23:42:51 -0700

    Revert to GitHub Pages configuration

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <main className='main'>
       <img 
-        src="/formal-pic-optimized.jpg" 
+        src="/portfolio/formal-pic-optimized.jpg" 
         alt="profile" 
         className='profile'
       />
--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Netlify doesn't need subdirectory
+  base: '/portfolio/',
   build: {
     assetsInlineLimit: 0, // Prevent inlining of assets
     rollupOptions: {
